98796019cc4afa9be940afa13d6d8328e3cf9999
Sonny <sunnydch999@gmail.com>
2017-04-16 15:10:14 +0100

    add load file problem

---
+++ test2/Assets/QuestionControll.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+
+public class QuestionControll : MonoBehaviour {
+    GameObject queLine0,queLine1,queLine2;
+    GameObject firstNum, secondNum, thridNum;
+    GameObject firstCube, secondCube, thirdCube;
+    string queNum = "";
+	// Use this for initialization
+	void Start () {
+
+        locateObject();
+
+        loadQuestionByFileName("test.txt");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void locateObject()
+    {
+        GameObject n = GameObject.Find("n");
+
+        if (n != null)
+        {
+            firstNum = n.transform.GetChild(0).gameObject;
+            secondNum = n.transform.GetChild(1).gameObject;
+            queLine0 = n.transform.GetChild(2).gameObject;
+            queLine1 = n.transform.GetChild(3).gameObject;
+            queLine2 = n.transform.GetChild(4).gameObject;
+
+        }
+
+        firstCube = GameObject.FindGameObjectWithTag("cube1");
+        secondCube = GameObject.FindGameObjectWithTag("cube2");
+
+    }
+    void loadQuestionByFileName(string name)
+    {
+        // Example #1
+        // Read the file as one string.
+        string path = "Assets/Resources/"+name;
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        string line = "";
+        line = reader.ReadLine();
+        Debug.Log(line);
+        queLine0.GetComponent<TextMesh>().text = line;
+        line = reader.ReadLine();
+        Debug.Log(line);
+        queLine1.GetComponent<TextMesh>().text = line;
+        line = reader.ReadLine();
+        Debug.Log(line);
+        queLine2.GetComponent<TextMesh>().text = line;
+        reader.Close();
+
+    }
+    
+}
